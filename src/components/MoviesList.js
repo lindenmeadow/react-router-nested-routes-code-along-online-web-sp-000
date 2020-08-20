f6b0a0772a2bdaba291e5121a45b1de65c151ea4
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-20 18:44:43 +0000

    Automatically backed up by Learn

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,7 +2,7 @@ const MoviesList = ({ movies }) => {
   const renderMovies = Object.keys(movies).map(movieID =>
     <Link key={movieID} to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
   );
- 
+
   return (
     <div>
       {renderMovies}
